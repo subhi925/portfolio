9409cf4095c6f967e1085cb1fcb17bd4df2cff44
subhi_925 <subhihamed88@gmail.com>
2025-11-10 06:49:22 +0200

    final

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 let projects = [];
 
 async function fetchProjects() {
-  const url = "https://subhiportfolio.ct.ws/loadproject.php";
+  const url = "https://subhimyportfolio.ct.ws/loadproject.php";
   try {
     const result = await fetch(url);
     if (result.ok) {
       projects = await result.json();
-      console.log("Projects fetched:", projects);
     }
   } catch (error) {
     console.error("Error fetching projects:", error);
   }
 }
 
-fetchProjects().then((projects) => {
-  console.log(projects);
-});
+const projectContainer = document.querySelector(".project");
+projectContainer.innerHTML = "";
+
+function displayProjects() {
+  fetchProjects().then(() => {
+    if (projects.length === 0) {
+      projectContainer.innerHTML = "<p>No projects found.</p>";
+      return;
+    }
+    projects.forEach((project) => {
+      projectContainer.innerHTML += `
+    <div class="profile">
+            <h2>${project.projectName}</h2>
+            <p>${project.description}</p>
+            <a id="project-link" href="${project.link}" target="_blank">${project.projectName}</a>
+          </div>
+    `;
+    });
+  });
+}
+displayProjects();
